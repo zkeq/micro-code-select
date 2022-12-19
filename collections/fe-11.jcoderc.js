0b0514164a2bc1a4281c3cdbedb168de650cc98f
zkeq <admin@icodeq.com>
2022-12-19 22:45:02 +0800

    more

--- collections/fe-11.jcoderc.js
+++ collections/fe-11.jcoderc.js
@@ -7,9 +7,9 @@ export default {
         folded: false,
         name: '11_CSS设置背景',
         collection: [
-          {name: '01_背景图片-background-image.html', author: 'Zkeq', id: '7178827427148202044', default: true},
+          {name: '01_背景图片-background-image.html', author: 'Zkeq', id: '7178827427148202044'},
           {name: '02_背景图片-background-repeat.html', author: 'Zkeq', id: '7178827657348382775'},
-          {name: '03_背景平铺-background-repeat练习.html', author: 'Zkeq', id: '7178827870876205117'},
+          {name: '03_背景平铺-background-repeat练习.html', author: 'Zkeq', id: '7178827870876205117', default: true},
           {name: '04_背景尺寸-background-size.html', author: 'Zkeq', id: '7178828028745613370'},
           {name: '05_背景位置-background-position.html', author: 'Zkeq', id: '7178828178314494013'},
           {name: '06_背景位置-background-position练习.html', author: 'Zkeq', id: '7178828301769637945'},
