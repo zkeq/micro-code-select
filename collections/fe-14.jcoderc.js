de1cd411f7a1e9baa0d20a7308b2a23872c0b700
zkeq <admin@icodeq.com>
2022-12-21 02:10:34 +0800

    文档修复

---
+++ collections/fe-14.jcoderc.js
@@ -0,0 +1,21 @@
+export default {
+    name: '22计6 前端 寒提资料',
+    url: 'https://code.onmicrosoft.cn/fe-14',
+    collection: [
+      {
+        type: 'folder',
+        folded: false,
+        name: '14_额外知识补充03',
+        collection: [
+          {name: '01_border的图形.html', author: 'Zkeq', id: '7179289483924209716'},
+          {name: '02_WebFonts的基本使用.html', author: 'Zkeq', id: '7179289595413004343'},   
+          {name: '03_WebFonts的兼容性写法.html', author: 'Zkeq', id: '7179289977207914555'},    
+          {name: '04_字体图标的使用.html', author: 'Zkeq', id: '7179290194271535136'},
+          {name: '05_使用iconfont配置的css.html', author: 'Zkeq', id: '7179290349783744564'},   
+          {name: '06_使用CSSSprite精灵图.html', author: 'Zkeq', id: '7179290579937787941'},   
+          {name: '07_光标的显示效果-cursor.html', author: 'Zkeq', id: '7179290973896179770'},        
+        ],
+      },
+      {name: '本站使用说明', id: '7178851964833235003'},
+    ],
+  };
---
+++ collections/fe-15.jcoderc.js
@@ -0,0 +1,21 @@
+export default {
+    name: '22计6 前端 寒提资料',
+    url: 'https://code.onmicrosoft.cn/fe-15',
+    collection: [
+      {
+        type: 'folder',
+        folded: false,
+        name: '15_布局-元素定位',
+        collection: [
+          {name: '01_标准流-normal-flow.html', author: 'Zkeq', id: '7179291234320515131'},
+          {name: '02_元素定位-相对定位-relative.html', author: 'Zkeq', id: '7179291391594332163'},   
+          {name: '03_元素定位-相对定位的练习.html', author: 'Zkeq', id: '7179291602328748072'},    
+          {name: '04_梦幻西游的练习-背景.html', author: 'Zkeq', id: '7179291720692006952'},
+          {name: '05_梦幻西游的练习-相对定位(较难).html', author: 'Zkeq', id: '7179291875583459339'},   
+          {name: '06_元素定位-固定定位-fixed.html', author: 'Zkeq', id: '7179292075639177253'},   
+          {name: '07_元素定位-固定定位的练习.html', author: 'Zkeq', id: '7179292219285700669'},      
+        ],
+      },
+      {name: '本站使用说明', id: '7178851964833235003'},
+    ],
+  };
